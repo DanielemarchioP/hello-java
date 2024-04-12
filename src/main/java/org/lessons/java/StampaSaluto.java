fa5786d4d6478e36a3d21cda5d8ce5e524d79a43
DanielemarchioP <danielemarchio709@gmail.com>
2024-04-13 01:12:51 +0200

    esercizio terminato

--- src/main/java/org/lessons/java/StampaSaluto.java
+++ src/main/java/org/lessons/java/StampaSaluto.java
@@ -3,7 +3,7 @@ package org.lessons.java;
 public class StampaSaluto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Ciao mamma non ci sto capendo molto xD");
 
 	}
 
